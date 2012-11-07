4649fd80e577c4203dbae0b887f9ed0a0ec0025e
SuryaBlack6 <b3ntol34@yahoo.com>
2012-11-07 17:19:00 +0700

    update configuration

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/git/';
+$config['base_url']	= 'http://localhost/ProjectGit/';
 
 /*
 |--------------------------------------------------------------------------
